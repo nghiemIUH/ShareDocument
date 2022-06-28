3e1b5cc2814b6436e101b7821f913778228dce99
nghiemIUH <vannghiem848@gmail.com>
2022-06-28 09:41:26 +0700

    update base site responsive

--- static/js/base.js
+++ static/js/base.js
@@ -7,4 +7,10 @@ if(pathName === '/'){
 
 function myFunction(x) {
     x.classList.toggle("change");
+    const menu = document.getElementById('menu')
+    if(menu.style.display === 'flex'){
+        menu.style.display = 'none'
+    }else{
+        menu.style.display = 'flex'
+    }
 }
