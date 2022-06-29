97761f63bbf9ef436642a324e615bcf42c16dcc9
nghiemIUH <vannghiem848@gmail.com>
2022-06-29 12:39:30 +0700

    update domain

--- ShareDocument/settings.py
+++ ShareDocument/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3q_-vyy%(t*v4)p=lbm^0mul$k3g#m9_zq+uq=c79i2%*ihtx!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['vndev.info']
 
 # Application definition
 
