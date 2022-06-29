409fd004d7548f32426e39209b7edf7777ef6e1c
nghiemIUH <vannghiem848@gmail.com>
2022-06-29 15:38:03 +0700

    config run.sh

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ sudo apt install python3-pip
 sudo apt install python3-venv
 python3 -m venv venv
 source venv/bin/activate
-pip3 install django
+pip3 install -r requirements.txt
 python3 manage.py collectstatic --noinput
 docker-compose up --build -d
