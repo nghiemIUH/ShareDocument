4e12dd127d64e5092745fa1c40b63f2e56c4e08b
nghiemIUH <vannghiem848@gmail.com>
2022-07-12 16:18:56 +0700

    update docker not build on server

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-sudo apt install python3-dev libpq-dev -y
-docker-compose up -d --build
+docker pull redis:latest
+docker pull postgres:latest
+docker pull nghiem0dang/sharedocument_nginx:latest
+docker pull nghiem0dang/sharedocument_frontend:latest
+docker pull nghiem0dang/sharedocument_web:latest
+
+# 
+docker run -d -p 6379:6379 --name redis --network redisnet redis:latest
+docker run -e POSTGRES_DB=sharedocument -e POSTGRES_PASSWORD=nghiem@123 \
+    -d  -p 5432:5432/tcp --expose 5432 --name db_production\
+    -v ../data:/var/lib/postgresql/data postgres:latest
