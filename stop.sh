ec72127e3538d44c31cdf1ba11db0dcba6cb9511
nghiemIUH <vannghiem848@gmail.com>
2022-07-20 20:34:17 +0700

    update nodejs

--- stop.sh
+++ stop.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 docker compose down
-docker volume rm sharedocument_backend_volume
-docker volume rm sharedocument_frontend_volume
+docker volume rm sharedocument_share_volume
 docker rmi sharedocument_nginx
 docker rmi sharedocument_web
 docker rmi sharedocument_frontend
