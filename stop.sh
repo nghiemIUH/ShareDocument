d9401fe7946a64663a5bc3b04aa994bcbf8bae37
nghiemIUH <vannghiem848@gmail.com>
2022-07-20 22:13:34 +0700

    update docker compose nodejs

--- stop.sh
+++ stop.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 docker compose down
-docker volume rm sharedocument_share_volume
+docker volume rm sharedocument_node
+docker volume rm sharedocument_react
+docker volume rm sharedocument_django
 docker rmi sharedocument_nginx
 docker rmi sharedocument_web
 docker rmi sharedocument_frontend
+docker rmi sharedocument_node
